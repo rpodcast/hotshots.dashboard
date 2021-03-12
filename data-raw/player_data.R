68057ce0b92d81f792d275ec5cc2d17d5e9f5eb4
Eric Nantz <theRcast@gmail.com>
2021-03-12 20:05:02 +0000

    add small updates to player data frame

--- data-raw/player_data.R
+++ data-raw/player_data.R
@@ -1,16 +1,17 @@
 ## code to prepare `player_data` dataset goes here
 
 player_data <- tibble::tribble(
-  ~player_name, ~full_name, ~country, ~avatar,
-  "Wimpy", "Martin Wimpress", "gb", "Wimpy.jpg",
-  "FrenchguyCH", "Yannick Mauray", "ch", "FrenchguyCH.jpg",
-  "rpodcast", "Eric Nantz", "us", "rpodcast.jpg",
-  "bigcalm", "", "gb", "bigcalm.png",
-  "TwoD", "", "se", "TwoD.png",
-  "madhens", "Monica Ayhens-Madon", "us", "madhens.jpg",
-  "TheUnwiseGeek", "", "us", "TheUnwiseGeek.png",
-  "popey", "Alan Pope", "gb", "popey.jpg",
-  "bigpod", "", "si", "bigpod.png"
+  ~player_name, ~full_name, ~country, ~avatar, ~plot_emoji,
+  "Wimpy", "Martin Wimpress", "gb", "Wimpy.jpg", "automobile",
+  "FrenchguyCH", "Yannick Mauray", "ch", "FrenchguyCH.jpg", "sport_utility_vehicle",
+  "rpodcast", "Eric Nantz", "us", "rpodcast.jpg", "racing_car",
+  "bigcalm", "", "gb", "bigcalm.png", "taxi",
+  "TwoD", "", "se", "TwoD.png", "articulated_lorry",
+  "madhens", "Monica Ayhens-Madon", "us", "madhens.jpg", "tractor",
+  "TheUnwiseGeek", "", "us", "TheUnwiseGeek.png", "police_car",
+  "popey", "Alan Pope", "gb", "popey.jpg", "bus",
+  "bigpod", "", "si", "bigpod.png", "minibus",
+  "crendo", "", "us", "", "motor_scooter"
 )
 
 
